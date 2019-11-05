a60fde771b11dcf2cbd90f87b3dbd7f6e039c2e5
Evan Chen <evan@evanchen.cc>
2019-11-05 17:18:02 -0500

    Bootstrap messages

--- hserve/settings.py
+++ hserve/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 	'compat',
 	'import_export',
 	'crispy_forms',
+	'bootstrap3',
 ]
 
 
@@ -131,7 +132,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
