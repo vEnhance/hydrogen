c4d73fa28c14617ac7ac8a517ced51c0e34a1b6c
Evan Chen <evan@evanchen.cc>
2019-11-05 17:49:13 -0500

    Put a CAPTCHA on creating a contest

--- hydrogen/forms.py
+++ hydrogen/forms.py
@@ -2,6 +2,24 @@ from django import forms
 from . import models
 
 class NewSubmissionKeyForm(forms.ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(NewSubmissionKeyForm, self).__init__(*args, **kwargs)
+		self.fields['captcha'] = forms.IntegerField(
+				required = True,
+				min_value = 0,
+				max_value = 100000000,
+				label = "Sum of first four primes",
+				help_text = "Are you human? "
+				"If so, please input the sum of the first four prime "
+				"numbers as a decimal number.")
+	def clean(self):
+		super(NewSubmissionKeyForm, self).clean()
+		seventeen = self.cleaned_data['captcha']
+		if seventeen != 17:
+			raise forms.ValidationError("You failed the CAPTCHA. "
+					"try entering the seventh prime instead.")
+		return self.cleaned_data
+
 	class Meta:
 		model = models.SubmissionKey
 		fields = ('display_name', 'real_name', 'email', )
