fd6fa55622b23d2946589b40a91b82704d20d746
Evan Chen <evan@evanchen.cc>
2020-02-08 21:58:39 -0500

    Improve about pages

--- core/views.py
+++ core/views.py
@@ -4,11 +4,17 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
 from . import models
+import hydrogen.models
 
 # Create your views here.
 
 class OrganizationDetailView(DetailView):
 	model = models.Organization
+	def get_context_data(self, **kwargs):
+		context = super(OrganizationDetailView, self).get_context_data(**kwargs)
+		context['num_active'] = hydrogen.models.Test.objects\
+				.filter(organization=self.object,visible=True).count()
+		return context
 class OrganizationListView(ListView):
 	model = models.Organization
 	def get_queryset(self):
